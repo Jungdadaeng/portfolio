428d7ab8075c995d374d45e6b088421163f079a5
jungdadaeng <jungdayoonyi@gmail.com>
2023-04-03 11:42:15 +0900

    link add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 import Layout from "./Layout/Layout";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/home/Home";
 import Profile from "./components/profile/Profile";
 import Projects from "./components/projects/Projects";
--- src/components/contect/Contect.js
+++ src/components/contect/Contect.js
@@ -2,8 +2,8 @@ import './Contect.css'
 export default function Contect(){
     return(
         <>
-            <div><i class="bi bi-github"></i> github 주소 : https://github.com/Jungdadaeng</div>
-            <div><i class="bi bi-file-earmark-text"></i> velog 주소 : https://velog.io/@jungdayoon</div>
+            <div><i className="bi bi-github"></i> github 주소 : <a href='https://github.com/Jungdadaeng' target='_blank' style={{textDecoration: 'none'}}>https://github.com/Jungdadaeng</a></div>
+            <div><i className="bi bi-file-earmark-text"></i> velog 주소 : <a href='https://velog.io/@jungdayoon' target='_blank'style={{textDecoration: 'none'}}>https://velog.io/@jungdayoon</a></div>
         </>
     )
 }
