f012f614e89255cfef7311d4b7c65c90b7640f11
jungdadaeng <jungdayoonyi@gmail.com>
2023-04-03 18:41:16 +0900

    projects edit

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,9 +5,8 @@ import Row from 'react-bootstrap/Row';
 export default function Project(){
     const projects = [
         {
-            title: '싸이월드 프로젝트',
-            contents: `React의 상태관리 방법 중 하나인 Redux를 사용하여 할일 목록을 관리할 수 있는 웹페이지를 개발하였습니다.
-            할일추가,수정,삭제,히스토리 조회 기능이 있습니다.`,
+            title: 'Cyworld Project',
+            contents: `React를 사용하여 싸이월드 홈페이지를 재현해보았습니다.`,
             src: '/images/cyworld.png',
         },
         {
@@ -18,14 +17,14 @@ export default function Project(){
         },
         {
             title: 'Sports Challenge Project',
-            contents: `React의 상태관리 방법 중 하나인 Redux를 사용하여 할일 목록을 관리할 수 있는 웹페이지를 개발하였습니다.
-            할일추가,수정,삭제,히스토리 조회 기능이 있습니다.`,
+            contents: `React의 상태관리 방법 중 하나인 Redux를 사용하여 스쿼트 개수를 관리할 수 있는 웹페이지를 개발하였습니다.
+            스쿼트 실행, 취소, 히스토리 조회 기능이 있습니다.`,
             src: '/images/wait.png',
         },
         {
-            title: '카카오 지도 API 연동 프로젝트',
-            contents: `React의 상태관리 방법 중 하나인 Redux를 사용하여 할일 목록을 관리할 수 있는 웹페이지를 개발하였습니다.
-            할일추가,수정,삭제,히스토리 조회 기능이 있습니다.`,
+            title: '맛집찾기 Project',
+            contents: `React,KakaoMap API를 사용하여 맛집을 마크할 수 있는 웹페이지를 개발하였습니다.
+            맛집추가,수정,삭제,히스토리 조회 기능이 있습니다.`,
             src: '/images/wait.png',
         },
     ]
